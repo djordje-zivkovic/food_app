89cb9dfbcf0f18f35ed2d56f864d0918e49b2a20
djordje-zivkovic <djordjezivkovic@protonmail.com>
2023-02-05 22:07:04 +0100

    add order module

--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { UsersService } from '../users/users.service';
+import { Order } from './order.entity';
 
 @Injectable()
-export class OrderService {}
+export class OrderService {
+  constructor(
+    @InjectRepository(Order) private repo: Repository<Order>,
+    private userService: UsersService,
+  ) {}
+}
