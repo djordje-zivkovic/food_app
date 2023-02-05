fab62fcf0d4adc37c76109981febbde6ce3efe1e
djordje-zivkovic <djordjezivkovic@protonmail.com>
2023-02-05 22:36:54 +0100

    fix roles

--- src/daily-menu/daily-menu.controller.ts
+++ src/daily-menu/daily-menu.controller.ts
@@ -1,4 +1,8 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { Roles } from '../decorators/role.decorator';
+import { Role } from '../enums/role.enum';
+import { RolesGuard } from '../guards/roles.guard';
 import { DailyMenuService } from './daily-menu.service';
 import { CreateDailyMenuDto } from './dtos/create-dailymenu.dto';
 
@@ -7,6 +11,8 @@ export class DailyMenuController {
   constructor(private dailyMenuService: DailyMenuService) {}
 
   @Post()
+  @Roles(Role.Owner)
+  @UseGuards(JwtAuthGuard, RolesGuard)
   createDailyMenu(@Body() body: CreateDailyMenuDto) {
     return this.dailyMenuService.create(body);
   }
--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -29,7 +29,7 @@ export class User {
   @Column({
     type: 'enum',
     enum: Role,
-    default: Role.Admin,
+    default: Role.Client,
   })
   role: Role;
 
