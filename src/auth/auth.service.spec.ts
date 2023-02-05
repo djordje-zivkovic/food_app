cd619da09658e59b0fc2d3e0758c861c8a3efd0e
djordje-zivkovic <djordjezivkovic@protonmail.com>
2023-02-05 16:41:46 +0100

    refactoring and test setup

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,12 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
-
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        {
+          provide: AuthService,
+          useValue: {},
+        },
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
@@ -15,4 +19,15 @@ describe('AuthService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('find users by their email', () => {});
+
+  //   it('should return user information if email and password are correct', async () => {
+  //     const email = 'admin@gmail.com';
+  //     const password = 'password';
+
+  //     const result = await service.validateUser(email, password);
+  //     expect(result).not.toBe('null');
+  //     expect(result).toMatchObject(['id', 'email']);
+  //   });
 });
