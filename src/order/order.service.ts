c3b5a9b97134d5fa0bcb489c818f470ec620da67
djordje-zivkovic <djordjezivkovic@protonmail.com>
2023-02-14 10:29:18 +0100

    fix dailymenus error handling

--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -3,7 +3,6 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { DailyMenuService } from '../daily-menu/daily-menu.service';
 import { MealService } from '../meal/meal.service';
-import { User } from '../users/user.entity';
 import { UsersService } from '../users/users.service';
 import { CreateOrderDto } from './dtos/create-order.dto';
 import { Order } from './order.entity';
@@ -13,7 +12,6 @@ export class OrderService {
   constructor(
     @InjectRepository(Order) private repo: Repository<Order>,
     private userService: UsersService,
-    private mealService: MealService,
     private dailyMenuService: DailyMenuService,
   ) {}
 
@@ -23,14 +21,19 @@ export class OrderService {
     order.dailyMenus = await this.dailyMenuService.getMenusById(
       orderDto.dailyMenuId,
     );
-    if (!order.dailyMenus) {
-      throw new NotFoundException('No meals found with the given id');
+    if (order.dailyMenus.length === 0) {
+      throw new NotFoundException('No menus found with the given id');
     }
-
+    console.log(order.dailyMenus);
     return this.repo.save(order);
   }
 
   async getAllOrders() {
-    return this.repo.find();
+    return this.repo.find({
+      relations: {
+        user: true,
+        dailyMenus: true,
+      },
+    });
   }
 }
